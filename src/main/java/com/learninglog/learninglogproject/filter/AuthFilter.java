59afcb3a8c34bd6ab921b6b9398d940a47a040c8
rojash03 <np05cp4a240283@iic.edu.np>
2026-04-20 19:46:46 +0545

    feat: Implement AuthFilter for request authentication and redirection

--- src/main/java/com/learninglog/learninglogproject/filter/AuthFilter.java
+++ src/main/java/com/learninglog/learninglogproject/filter/AuthFilter.java
@@ -1,4 +1,38 @@
 package com.learninglog.learninglogproject.filter;
 
-public class AuthFilter {
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class AuthFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        HttpSession Session = httpRequest.getSession(false);
+
+        boolean isLoggedIn = ( Session != null && Session.getAttribute("user") != null);
+
+        String URI = httpRequest.getRequestURI();
+
+        boolean isLoginPageOrRegisterPage = URI.endsWith("login") || URI.endsWith("register");
+
+        if (isLoggedIn && isLoginPageOrRegisterPage) {
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/");
+            return;
+        }
+        if (!isLoggedIn || !isLoginPageOrRegisterPage) {
+            chain.doFilter(request, response);
+            else{
+                httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
+            }
+        }
+    }
 }
